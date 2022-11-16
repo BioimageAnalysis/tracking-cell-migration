75668653c8d31299987ea5a11efffda3de48d1c1
Jian Tay <jwtay.physics@gmail.com>
2022-11-15 18:13:56 -0700

    Added tracking

--- readImage.m
+++ readImage.m
@@ -1,4 +1,12 @@
 function img = readImage(dataDir, row, col, field, channel, frame)
+% r03c03f01p01-ch1sk1fk1fl1.tiff
+% 
+% Where
+% r (01-08) = row
+% c (01-12) = column
+% f (01-09) = field
+% p (01-21) = plane
+% ch (1-4) = channel
 
 filename = ...
     sprintf('r%02.0fc%02.0ff%02.0fp01-ch%.0fsk%.0ffk1fl1.tiff', ...
--- testTracking.m
+++ testTracking.m
@@ -3,21 +3,54 @@ clc
 
 dataDir = 'H:\HaCaT movies-20221101\QB-20221028-HaCaT__2022-10-28T16_46_24-Measurement 2\Images';
 
-I = imread(fullfile(dataDir, 'r04c06f01p01-ch1sk1fk1fl1.tiff'));
+LAP = LAPLinker;
+LAP.TrackDivision = true;
+LAP.DivisionScoreRange = [0 20];
 
-I2 = imread(fullfile(dataDir, 'r04c06f01p01-ch1sk2fk1fl1.tiff'));
+vid = VideoWriter('test.avi');
+vid.FrameRate = 5;
+open(vid)
 
-figure;
-imshow(imadjust(I), [])
+for iT = 1:102
+    
+    I = readImage(dataDir, 4, 6, 1, 1, iT);
+    
+    %%
+    mask = imbinarize(I, 'adaptive', 'Sensitivity', 0.2);
+    mask = imclose(mask, strel('disk', 2));
+    mask = imopen(mask, strel('disk', 1));
+    
+    mask = imclearborder(mask);
+    mask = bwareaopen(mask, 50);
+    
+    dd = -bwdist(~mask);
+    dd(~mask) = false;
+    dd = imhmin(dd, 1);
+    
+    L = watershed(dd);
+    
+    mask(L == 0) = false;
+    
+    data = regionprops(mask, 'Centroid');
+    
+    LAP = assignToTrack(LAP, iT, data);
+    
+    Iout = double(I);
+    Iout = (Iout - min(Iout, [], 'all'))/(max(Iout, [], 'all') - min(Iout, [], 'all'));
+    
+    Iout = showoverlay(Iout, bwperim(mask));
+    for iAT = LAP.activeTrackIDs
+        ct = getTrack(LAP, iAT);
+        Iout = insertText(Iout, ct.Centroid(end, :), int2str(iAT), ...
+            'TextColor', 'white', 'BoxOpacity', 0);
+    end
+    
+    writeVideo(vid, Iout);
+    
+end
+close(vid)
 
-figure;
-imshow(imadjust(I2), [])
+% imshowpair(imadjust(I), bwperim(mask))
 
-% r03c03f01p01-ch1sk1fk1fl1.tiff
-% 
-% Where
-% r (01-08) = row
-% c (01-12) = column
-% f (01-09) = field
-% p (01-21) = plane
-% ch (1-4) = channel
+
+
