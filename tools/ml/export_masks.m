a4c6c9d0c927762e3f738f44bae8de3db1c35454
Jian Wei Tay <jian.tay@colorado.edu>
2025-01-08 12:22:28 -0700

    Added script to export masks for ML

---
+++ tools/ml/export_masks.m
@@ -0,0 +1,40 @@
+clearvars
+clc
+
+dataDir = 'Z:\Datasets\QB-Hacat-EKAR-H2BmCherry-11012024__2024-11-01T16_56_55-Measurement 1\Images';
+
+exportDir = 'Z:\ML data';
+
+imgFiles = dir(fullfile(dataDir, '*-ch3*.tiff'));
+
+%%
+%Pick some random images
+idx = randsample(numel(imgFiles), 3);
+
+imgList = {};
+
+for ii = 1
+
+    imgList{ii} = imgFiles(ii).name;
+
+    I = imread(fullfile(dataDir, imgFiles(idx(ii)).name));
+
+    mask = ECMCellTracker.segmentCells(I, 20);
+
+    [~, fn] = fileparts(imgFiles(ii).name);
+
+    imwrite(I, fullfile(exportDir, 'images', [fn, '.tiff']), 'Compression', 'none')
+    imwrite(mask, fullfile(exportDir, 'masks', [fn, '.tiff']), 'Compression', 'none')
+    
+end
+
+%%
+fid = fopen(fullfile(exportDir, 'files.csv'), 'w');
+
+for iF = 1:numel(imgList)
+
+    fprintf(fid, '%s\n', imgList{iF});
+
+end
+
+fclose(fid)
